476ab5c3dcc79e64f0a07deae5a10584dfeef08a
Vadim Storozhuk <vadozy@yahoo.com>
2020-11-08 00:07:56 -0500

    Inside Section 20 [ End of 75 ]

--- main-project/frontend/src/App.js
+++ main-project/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.scss';
 import Navbar from './components/Navbar';
 import Notes from './components/Notes';
@@ -6,9 +6,11 @@ import Notes from './components/Notes';
 import M from 'materialize-css';
 
 function App() {
+  const _noteModalRef = useRef(null);
   // initializes materialized effects
   useEffect(() => {
     M.AutoInit();
+    M.Modal.init(_noteModalRef.current);
     return () => {};
   }, []);
 
@@ -16,6 +18,33 @@ function App() {
     <div className='App'>
       <Navbar />
       <Notes />
+      <div className='fixed-action-btn'>
+        <a
+          href='#modal-add-note'
+          className='btn-floating btn-large waves-effect waves-light red lighten-1 modal-trigger'
+        >
+          <i className='material-icons'>add</i>
+        </a>
+      </div>
+      <div id='modal-add-note' className='modal' ref={_noteModalRef}>
+        <div className='modal-content'>
+          <h4>Create a new note</h4>
+        </div>
+        <div className='modal-footer'>
+          <a
+            href='#!'
+            className='btn-flat modal-action modal-close waves-effect red white-text'
+          >
+            Cancel
+          </a>{' '}
+          <a
+            href='#!'
+            className='btn-flat modal-action modal-close waves-effect green white-text'
+          >
+            Ok
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
